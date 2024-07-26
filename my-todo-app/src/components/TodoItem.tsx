a072da29021f6f434f266496dedb0d418ea75e6e
gayoung lee <ken604@naver.com>
2024-07-26 17:32:02 +0900

    중간업데이트

--- my-todo-app/src/components/TodoItem.tsx
+++ my-todo-app/src/components/TodoItem.tsx
@@ -1,35 +1,140 @@
-import React from "react";
-import { Task } from "../types/task";
+import React, { useState } from "react";
+import { Task, Category } from "../types/task";
 import "../styles/TodoItem.scss";
-//아이콘
 import { FaCheck } from "react-icons/fa6";
 import { RiDeleteBin2Fill } from "react-icons/ri";
 
 interface TodoItemProps {
+  activeCategory: Category;
   tasks: Task[];
+  setTasks: React.Dispatch<React.SetStateAction<Task[]>>;
 }
 
-const TodoItem: React.FC<TodoItemProps> = ({ tasks }) => {
+const TodoItem: React.FC<TodoItemProps> = ({
+  activeCategory,
+  tasks,
+  setTasks,
+}) => {
+  const [newTask, setNewTask] = useState<string>("");
+  const [activeTab, setActiveTab] = useState<string>("all");
+
+  const handleAddTask = () => {
+    if (newTask.trim() !== "") {
+      const newTaskItem: Task = {
+        id: tasks.length + 1,
+        text: newTask,
+        completed: false,
+        categoryId: activeCategory.id,
+      };
+      setTasks([...tasks, newTaskItem]);
+      setNewTask("");
+    }
+  };
+
+  const handleTaskInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setNewTask(e.target.value);
+  };
+
+  const handleTabClick = (tab: string) => {
+    setActiveTab(tab);
+  };
+
+  const getFilteredTasks = () => {
+    const categoryTasks = tasks.filter(
+      (task) => task.categoryId === activeCategory.id
+    );
+    if (activeTab === "done") {
+      return categoryTasks.filter((task) => task.completed);
+    } else if (activeTab === "notDone") {
+      return categoryTasks.filter((task) => !task.completed);
+    }
+    return categoryTasks;
+  };
+
   return (
-    <div>
-      {tasks.map((task) => (
-        <div key={task.id} className="task_container">
-          <div className="taskWrap">
-            <div className="taskContent">
-              <p>{task.text}</p>
+    <section className="taskSection">
+      <div className="categoryWrap">
+        <a>{activeCategory.text}</a>
+        <div className="taskAdd">
+          <input
+            type="text"
+            placeholder="Enter New Task"
+            value={newTask}
+            onChange={handleTaskInputChange}
+          />
+          <button onClick={handleAddTask}>+</button>
+        </div>
+      </div>
+      {getFilteredTasks().length === 0 ? (
+        <p className="taskNone">할 일을 추가해주세요</p>
+      ) : (
+        <div className="tabWrap">
+          <div className="tabContainer">
+            <div
+              className={`tabStyle ${activeTab === "all" ? "active" : ""}`}
+              id="all"
+              onClick={() => handleTabClick("all")}
+            >
+              <div className="taskNum">{getFilteredTasks().length}</div>
+              <a>All</a>
             </div>
-            <div className="taskBtnWrap">
-              <button className="taskBtnStyle" id="edit">
-                <FaCheck />
-              </button>
-              <button className="taskBtnStyle" id="delete">
-                <RiDeleteBin2Fill />
-              </button>
+          </div>
+          <div className="tabContainer">
+            <div
+              className={`tabStyle ${activeTab === "done" ? "active" : ""}`}
+              id="done"
+              onClick={() => handleTabClick("done")}
+            >
+              <div className="taskNum">
+                {
+                  tasks.filter(
+                    (task) =>
+                      task.categoryId === activeCategory.id && task.completed
+                  ).length
+                }
+              </div>
+              <a>Done</a>
+            </div>
+          </div>
+          <div className="tabContainer">
+            <div
+              className={`tabStyle ${activeTab === "notDone" ? "active" : ""}`}
+              id="notDone"
+              onClick={() => handleTabClick("notDone")}
+            >
+              <div className="taskNum">
+                {
+                  tasks.filter(
+                    (task) =>
+                      task.categoryId === activeCategory.id && !task.completed
+                  ).length
+                }
+              </div>
+              <a>Not Done</a>
             </div>
           </div>
         </div>
-      ))}
-    </div>
+      )}
+      <div>
+        {getFilteredTasks().map((task) => (
+          <div key={task.id} className="task_container">
+            <div className="taskWrap">
+              <div className="taskContent">
+                <p>{task.text}</p>
+              </div>
+              <div className="taskBtnWrap">
+                <button className="taskBtnStyle" id="edit">
+                  <FaCheck />
+                </button>
+                <button className="taskBtnStyle" id="delete">
+                  <RiDeleteBin2Fill />
+                </button>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </section>
   );
 };
 
--- my-todo-app/src/components/TodoList.tsx
+++ my-todo-app/src/components/TodoList.tsx
@@ -1,28 +1,21 @@
 import React, { useState } from "react";
 import "../styles/TodoList.scss";
 import TodoItem from "./TodoItem";
+import HeaderSection from "./HeaderSection";
+import CategorySection from "./CategorySection";
 import { Category, Task } from "../types/task";
-//아이콘
-import { RiDeleteBin2Fill } from "react-icons/ri";
 
 const TodoList: React.FC = () => {
   const [isInputVisible, setIsInputVisible] = useState<boolean>(false);
-  const [activeTab, setActiveTab] = useState<string>("all");
   const [categories, setCategories] = useState<Category[]>([]);
   const [newCategory, setNewCategory] = useState<string>("");
   const [activeCategory, setActiveCategory] = useState<Category | null>(null);
-  const [tasks, setTasks] = useState<Task[]>([]); // 할 일 목록
+
+  // task 관련
+  const [tasks, setTasks] = useState<Task[]>([]);
 
   const toggleInput = (): void => {
     setIsInputVisible(!isInputVisible);
-  };
-
-  const handleTabClick = (tab: string) => {
-    setActiveTab(tab);
-  };
-
-  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setNewCategory(e.target.value);
   };
 
   const handleAddCategory = () => {
@@ -34,155 +27,38 @@ const TodoList: React.FC = () => {
       const updatedCategories = [...categories, newCat];
       setCategories(updatedCategories);
       setNewCategory("");
-      setActiveCategory(updatedCategories[0]); // 첫 번째 카테고리를 활성화
+      setActiveCategory(updatedCategories[0]);
+      setIsInputVisible(false); // 카테고리 추가 후 input 숨김
     }
-  };
-
-  const handleCategoryClick = (category: Category) => {
-    setActiveCategory(category);
-  };
-
-  const handleDeleteCategory = () => {
-    if (activeCategory) {
-      setCategories(categories.filter((cat) => cat.id !== activeCategory.id));
-      setActiveCategory(categories.length > 1 ? categories[0] : null); // 삭제 후 첫 번째 카테고리 또는 null 설정
-    }
-  };
-
-  const getTasksForCategory = (categoryId: number) => {
-    return tasks.filter((task) => task.categoryId === categoryId);
   };
 
   return (
     <div className="todoList_container">
-      <section className="titleSection">
-        <div className="title">
-          <a>To Do List</a>
-          <div className="addBtn" id="titleAdd">
-            <button
-              onClick={toggleInput}
-              className={isInputVisible ? "rotate" : ""}
-            >
-              +
-            </button>
-          </div>
-        </div>
-        <div className={`inputContainer ${isInputVisible ? "show" : ""}`}>
-          <div className="category_inputWrap">
-            <input
-              type="text"
-              placeholder="Enter New Category"
-              value={newCategory}
-              onChange={handleInputChange}
+      <HeaderSection
+        newCategory={newCategory}
+        setNewCategory={setNewCategory}
+        handleAddCategory={handleAddCategory}
+        isInputVisible={isInputVisible}
+        toggleInput={toggleInput}
+        categories={categories}
+      />
+      {categories.length > 0 && (
+        <div className="column">
+          <CategorySection
+            categories={categories}
+            activeCategory={activeCategory}
+            setActiveCategory={setActiveCategory}
+            setCategories={setCategories}
+          />
+          {activeCategory && (
+            <TodoItem
+              activeCategory={activeCategory}
+              tasks={tasks}
+              setTasks={setTasks}
             />
-            <button onClick={handleAddCategory}>Add</button>
-          </div>
+          )}
         </div>
-      </section>
-      <div className="column">
-        <section className="categorySection">
-          {categories.length === 0 ? (
-            <p className="categoryNone">카테고리를 추가해주세요</p>
-          ) : (
-            <div className="category">
-              {categories.map((category) => (
-                <div key={category.id} className="categoryItemWrapper">
-                  <a
-                    className={`categoryItem ${
-                      activeCategory?.id === category.id ? "active" : ""
-                    }`}
-                    onClick={() => handleCategoryClick(category)}
-                  >
-                    {category.text}
-                  </a>
-                  {activeCategory?.id === category.id && (
-                    <div
-                      className="categoryDelete"
-                      id="categoryDelete"
-                      onClick={handleDeleteCategory}
-                    >
-                      <RiDeleteBin2Fill />
-                    </div>
-                  )}
-                </div>
-              ))}
-            </div>
-          )}
-        </section>
-        <section className="taskSection">
-          {activeCategory ? (
-            <>
-              <div className="categoryWrap">
-                <a>{activeCategory.text}</a>
-                <div className="taskAdd">
-                  <button>+</button>
-                </div>
-              </div>
-              {getTasksForCategory(activeCategory.id).length === 0 ? (
-                <p className="taskNone">할 일을 추가해주세요</p>
-              ) : (
-                <div className="tabWrap">
-                  <div className="tabContainer">
-                    <div
-                      className={`tabStyle ${
-                        activeTab === "all" ? "active" : ""
-                      }`}
-                      id="all"
-                      onClick={() => handleTabClick("all")}
-                    >
-                      <div className="taskNum">
-                        {getTasksForCategory(activeCategory.id).length}
-                      </div>
-                      <a>All</a>
-                    </div>
-                  </div>
-                  <div className="tabContainer">
-                    <div
-                      className={`tabStyle ${
-                        activeTab === "done" ? "active" : ""
-                      }`}
-                      id="done"
-                      onClick={() => handleTabClick("done")}
-                    >
-                      <div className="taskNum">
-                        {
-                          getTasksForCategory(activeCategory.id).filter(
-                            (task) => task.completed
-                          ).length
-                        }
-                      </div>
-                      <a>Done</a>
-                    </div>
-                  </div>
-                  <div className="tabContainer">
-                    <div
-                      className={`tabStyle ${
-                        activeTab === "notDone" ? "active" : ""
-                      }`}
-                      id="notDone"
-                      onClick={() => handleTabClick("notDone")}
-                    >
-                      <div className="taskNum">
-                        {
-                          getTasksForCategory(activeCategory.id).filter(
-                            (task) => !task.completed
-                          ).length
-                        }
-                      </div>
-                      <a>Not Done</a>
-                    </div>
-                  </div>
-                </div>
-              )}
-              {getTasksForCategory(activeCategory.id).length > 0 && (
-                <TodoItem tasks={getTasksForCategory(activeCategory.id)} />
-              )}
-            </>
-          ) : (
-            <p className="taskNone">카테고리를 추가해주세요</p>
-          )}
-        </section>
-      </div>
+      )}
     </div>
   );
 };
